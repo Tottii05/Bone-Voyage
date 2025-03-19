b4afc6f523cddbc937917c2f315b85f24c2a8f1b
The Princess of La Manchaland <104784400+JoaquinAlcazar@users.noreply.github.com>
2025-03-19 17:43:35 +0100

    Arquera 0.1
    Cosas de la arquera i guess
    Y las escena del testing de la mujer flechas. Espero no destruir el proyecto

--- BoneVoyage/Assets/Scripts/Characters/Rogue.cs
+++ BoneVoyage/Assets/Scripts/Characters/Rogue.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,12 @@ using UnityEngine;
 public class Rogue : ACharacter
 {
     public GameObject bulletPrefab;
+    public GameObject grenadePrefab;
     public Transform spawnPoint;
+    public bool isAttacking = false;
+    public bool isRolling = false;
+    public bool usingSpecial = false;
+    public bool specialCD = false;
 
     public void Start()
     {
@@ -16,23 +22,81 @@ public class Rogue : ACharacter
 
     public override void Attack()
     {
-        StartCoroutine(PerformAttack());
+        if (!isAttacking) StartCoroutine(PerformAttack()); 
     }
     public override void Support()
     {
+        if (!isRolling) StartCoroutine(PerformRoll());
     }
     public override void Special()
     {
+        if (!usingSpecial) StartCoroutine(useEspecial());
     }
     public IEnumerator PerformAttack()
-    {
-        characterBehaviour.isWaiting = true;
-        animator.SetTrigger("Attack");
-        yield return new WaitForSeconds(0.7f);
-        if (bulletPrefab != null)
+    { 
+        isAttacking = true;
+        if (!usingSpecial)
         {
-            Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+            animator.SetTrigger("Attack");
+            yield return new WaitForSeconds(0.7f);
+            if (bulletPrefab != null)
+            {
+                Instantiate(bulletPrefab, spawnPoint.position, spawnPoint.rotation);
+            }
+        } else
+        {
+            animator.SetTrigger("Attack");
+            yield return new WaitForSeconds(0.7f);
+            if (bulletPrefab != null)
+            {
+                Instantiate(grenadePrefab, spawnPoint.position, spawnPoint.rotation);
+            }
         }
-        characterBehaviour.isWaiting = false;
+        
+        isAttacking = false;
+    }
+
+    public IEnumerator PerformRoll()
+    {
+        Debug.Log("Roll");
+        isRolling = true;
+        animator.SetTrigger("Dash");
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        rb.detectCollisions = false;
+        rb.velocity = Vector3.zero;
+
+        Vector3 startPos = transform.position;
+        Vector3 targetPos = startPos + transform.forward * 3f; // Ajusta el valor de 5f para la distancia del roll.
+
+        float duration = 0.5f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            rb.MovePosition(Vector3.Lerp(startPos, targetPos, elapsedTime / duration));
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        rb.MovePosition(targetPos);
+        rb.detectCollisions = true;
+        gameObject.GetComponent<CapsuleCollider>().enabled = true;
+        yield return new WaitForSeconds(2f);
+
+        isRolling = false;
+        
+    }
+
+    public IEnumerator useEspecial()
+    {
+        usingSpecial = true;
+        specialCD = true;
+        yield return new WaitForSeconds(5f);
+        usingSpecial = false;
+        yield return new WaitForSeconds(10f);
+        specialCD = false;
     }
 }
