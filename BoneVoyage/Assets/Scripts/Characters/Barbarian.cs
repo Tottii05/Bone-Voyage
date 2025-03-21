3e69db987471e9a6f19970faceae162517810b6f
RaulPalomo <raul.palomo.7e7@itb.cat>
2025-03-21 17:46:13 +0100

    barbarian animations

--- BoneVoyage/Assets/Scripts/Characters/Barbarian.cs
+++ BoneVoyage/Assets/Scripts/Characters/Barbarian.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class Barbarian : ACharacter
 {
     public BoxCollider attackCollider;
+    public GameObject mug;
+    public List<GameObject> weaponsLeft ;
+    public List<GameObject> weaponsRight;
+    public GameObject currentWeaponLeft;
+    public GameObject currentWeaponRight;
+    public int damageAugment = 5;
+    public int speedAugment = 2;
+    public int heal=20;
+    public float mugbuffDuration = 5f;
     public void Start()
     {
         characterBehaviour = GetComponent<CharacterBehaviour>();
@@ -18,16 +27,95 @@ public class Barbarian : ACharacter
     }
     public override void Support()
     {
+        StartCoroutine(PerformSupport());
     }
     public override void Special()
     {
+        StartCoroutine(PerformSpecial());
     }
     public IEnumerator PerformAttack()
     {
-        characterBehaviour.isWaiting = true;
-        animator.SetTrigger("attack");
+        characterBehaviour.isWaiting = false;
+        animator.SetTrigger("Attack");
+        StartCoroutine(Combo());
+
         yield return new WaitForSeconds(0.7f);
 
+        characterBehaviour.isWaiting = true;
+    }
+
+    public IEnumerator PerformSupport()
+    {
         characterBehaviour.isWaiting = false;
+        animator.SetTrigger("Support");
+        mug.SetActive(true);
+        currentWeaponLeft.SetActive(false);
+        currentWeaponRight.SetActive(false);
+        
+        yield return new WaitForSeconds(2f);
+        
+        StartCoroutine(MugBuff());
+        mug.SetActive(false);
+        currentWeaponLeft.SetActive(true);
+        currentWeaponRight.SetActive(true);
+        characterBehaviour.isWaiting = true;
+    }
+
+    public IEnumerator PerformSpecial()
+    {
+        characterBehaviour.isWaiting = false;
+        animator.SetTrigger("Special");
+
+        yield return new WaitForSeconds(0.7f);
+
+        characterBehaviour.isWaiting = true;
+    }
+    public IEnumerator Hit()
+    {
+        characterBehaviour.isWaiting = false;
+        animator.SetTrigger("Hit");
+
+        yield return new WaitForSeconds(0.3f);
+
+        characterBehaviour.isWaiting = true;
+    }
+    public IEnumerator Combo()
+    {
+        int num=animator.GetInteger("AttackNum");
+        switch (num)
+        {
+            case 0:
+                animator.SetInteger("AttackNum", 1);
+                break;
+            case 1:
+                animator.SetInteger("AttackNum", 2);
+                break;
+            case 2:
+                animator.SetInteger("AttackNum", 3);
+                break;
+            case 3:
+                animator.SetInteger("AttackNum", 1);
+                break;
+        }
+        
+        yield return new WaitForSeconds(1.5f);
+        if(num+1== animator.GetInteger("AttackNum"))
+        {
+            animator.SetInteger("AttackNum", 0);
+        }
+        
+    }
+    public IEnumerator MugBuff()
+    {
+        health += heal;
+        speed += speedAugment;
+        yield return new WaitForSeconds(mugbuffDuration);
+        speed -= speedAugment;
+    }
+    public new void TakeDamage(float damage)
+    {
+        
+        base.TakeDamage(damage);
+        StartCoroutine(Hit());
     }
 }
