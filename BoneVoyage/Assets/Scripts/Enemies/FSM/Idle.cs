16c548e545596ef64f49491d0e96c1629203abd8
Tottii05 <146985184+Tottii05@users.noreply.github.com>
2025-03-27 12:30:41 +0100

    Enemy melee animations done , enemy behaviour 70%  done

--- BoneVoyage/Assets/Scripts/Enemies/FSM/Idle.cs
+++ BoneVoyage/Assets/Scripts/Enemies/FSM/Idle.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(fileName = "Patrol", menuName = "StatesSO/Patrol")]
-public class Patrol : StateSO
+[CreateAssetMenu(fileName = "Idle", menuName = "StatesSO/Idle")]
+public class Idle : StateSO
 {
     public override void OnStateEnter(EnemyController ec)
     {
-        ec.gameObject.GetComponent<EnemyPathFinding>().destination = GameObject.Find("BPoint");
+
     }
 
     public override void OnStateExit(EnemyController ec)
@@ -16,6 +16,6 @@ public class Patrol : StateSO
 
     public override void OnStateUpdate(EnemyController ec)
     {
-        ec.gameObject.GetComponent<EnemyPathFinding>().Patrol();
+
     }
 }
