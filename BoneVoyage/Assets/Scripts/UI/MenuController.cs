0f27e7309f0d56b0c546c14924e8ae510bbd7807
JoaquinAlcazar <qihang.li.7e7@itb.cat>
2025-04-08 15:46:22 +0200

    funcionalidades menu

--- BoneVoyage/Assets/Scripts/UI/MenuController.cs
+++ BoneVoyage/Assets/Scripts/UI/MenuController.cs
@@ -2,13 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuController : SceneController
 {
-    // Start is called before the first frame update
+    public GameObject panelOptions;
+    public GameObject buttonEnter;
+    public TextMeshProUGUI textTitle;
     void Start()
     {
-        
+        if(PlayerPrefs.GetInt("FirstTime",0) == 0)
+        {
+            panelOptions.SetActive(true);
+            buttonEnter.SetActive(true);
+        }
+        else
+        {
+            panelOptions.SetActive(false);
+            buttonEnter.SetActive(false);
+        }
+    }
+    public void ButtonPress()
+    {
+        this.gameObject.SetActive(false);
+    }
+    public void MoveTitle()
+    {
+
     }
 
     // Update is called once per frame
