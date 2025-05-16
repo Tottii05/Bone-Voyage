9d19530db028c8de80e1addc9e551787a134c8eb
Tottii05 <146985184+Tottii05@users.noreply.github.com>
2025-05-16 17:41:36 +0200

    money glitch remove

--- BoneVoyage/Assets/Scripts/UI/CoinUI.cs
+++ BoneVoyage/Assets/Scripts/UI/CoinUI.cs
@@ -28,21 +28,4 @@ public class CoinUI : MonoBehaviour
         int coins = PlayerPrefs.GetInt("Coins", 0);
         coinText.text = coins.ToString();
     }
-
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            PlayerPrefs.SetInt("Coins", 0);
-            PlayerPrefs.Save();
-            UpdateCoinText();
-        }
-
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            PlayerPrefs.SetInt("Coins", 5);
-            PlayerPrefs.Save();
-            UpdateCoinText();
-        }
-    }
 }
